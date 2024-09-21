4926aade8497f9c06416c262f94131c625b58b59
David Higgins <david14higgins@gmail.com>
2024-09-21 15:56:49 +0100

    remove init with zeros method and refactor

--- src/Main.java
+++ src/Main.java
@@ -1,149 +1,107 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
-//        int[][] matrix_A = { { 2, 4, 6, 8, 10, 12, 14, 16 },
-//                { 1, 3, 5, 7, 9, 11, 13, 15 },
-//                { 4, 6, 8, 10, 12, 14, 16, 2 },
-//                { 3, 5, 7, 9, 11, 13, 15, 1 },
-//                { 6, 8, 10, 12, 14, 16, 2, 4 },
-//                { 5, 7, 9, 11, 13, 15, 1, 3 },
-//                { 8, 10, 12, 14, 16, 2, 4, 6 },
-//                { 7, 9, 11, 13, 15, 1, 3, 5 }};
-
-        int[][] matrix_A = readMatrixFromFile("8x8_Matrix_A.txt");
+        int[][] matrixA = readMatrixFromFile("8x8_Matrix_A.txt");
 
         System.out.println("Array A =>");
-        printMat(matrix_A);
+        printMatrix(matrixA);
 
-//        int[][] matrix_B = { { 10, 12, 14, 16, 2, 4, 6, 8 },
-//                { 9, 11, 13, 15, 1, 3, 5, 7 },
-//                { 12, 14, 16, 2, 4, 6, 8, 10 },
-//                { 11, 13, 15, 1, 3, 5, 7, 9 },
-//                { 14, 16, 2, 4, 6, 8, 10, 12 },
-//                { 13, 15, 1, 3, 5, 7, 9, 11 },
-//                { 16, 2, 4, 6, 8, 10, 12, 14 },
-//                { 15, 1, 3, 5, 7, 9, 11, 13 }};
-
-        int[][] matrix_B = readMatrixFromFile("8x8_Matrix_B.txt");
+        int[][] matrixB = readMatrixFromFile("8x8_Matrix_B.txt");
 
         System.out.println("Array B =>");
-        printMat(matrix_B);
+        printMatrix(matrixB);
 
-        int[][] result_matrix =  multiply_matrix(matrix_A, matrix_B);
+        int[][] resultMatrix =  multiplyMatrix(matrixA, matrixB);
 
         System.out.println("Result Array =>");
-        printMat(result_matrix);
+        printMatrix(resultMatrix);
     }
-    private static int[][] multiply_matrix(int[][] matrix_A,int[][] matrix_B)
+    private static int[][] multiplyMatrix(int[][] matrixA,int[][] matrixB)
     {
-        int col_1 = matrix_A[0].length;
-        int row_1 = matrix_A.length;
-        int col_2 = matrix_B[0].length;
-        int row_2 = matrix_B.length;
+        int col1 = matrixA[0].length;
+        int row1 = matrixA.length;
+        int col2 = matrixB[0].length;
+        int row2 = matrixB.length;
 
-        if (col_1 != row_2) {
+        if (col1 != row2) {
             System.out.println("\nError: The number of columns in Matrix A  must be equal to the number of rows in Matrix B\n");
             int[][] temp = new int[1][1];
             temp[0][0]=0;
             return temp;
         }
 
-        int[][] result_matrix = new int[row_1][col_2];
-        initWithZeros(result_matrix,row_1,col_2);
+        int[][] resultMatrix = new int[row1][col2];
 
-        if (col_1 == 1){
-            result_matrix[0][0] = matrix_A[0][0] * matrix_B[0][0];
+        if (col1 == 1){
+            resultMatrix[0][0] = matrixA[0][0] * matrixB[0][0];
         }else {
-            int split_index = col_1 / 2;
+            int splitIndex = col1 / 2;
 
-            int[][] result_matrix_00 = new int[split_index][split_index];
-            int[][] result_matrix_01 = new int[split_index][split_index];
-            int[][] result_matrix_10 = new int[split_index][split_index];
-            int[][] result_matrix_11 = new int[split_index][split_index];
-            initWithZeros(result_matrix_00,split_index,split_index);
-            initWithZeros(result_matrix_01,split_index,split_index);
-            initWithZeros(result_matrix_10,split_index,split_index);
-            initWithZeros(result_matrix_11,split_index,split_index);
+            int[][] resultMatrix_00 = new int[splitIndex][splitIndex];
+            int[][] resultMatrix_01 = new int[splitIndex][splitIndex];
+            int[][] resultMatrix_10 = new int[splitIndex][splitIndex];
+            int[][] resultMatrix_11 = new int[splitIndex][splitIndex];
 
-            int[][] a00 = new int[split_index][split_index];
-            int[][] a01 = new int[split_index][split_index];
-            int[][] a10 = new int[split_index][split_index];
-            int[][] a11 = new int[split_index][split_index];
-            int[][] b00 = new int[split_index][split_index];
-            int[][] b01 = new int[split_index][split_index];
-            int[][] b10 = new int[split_index][split_index];
-            int[][] b11 = new int[split_index][split_index];
-            initWithZeros(a00,split_index,split_index);
-            initWithZeros(a01,split_index,split_index);
-            initWithZeros(a10,split_index,split_index);
-            initWithZeros(a11,split_index,split_index);
-            initWithZeros(b00,split_index,split_index);
-            initWithZeros(b01,split_index,split_index);
-            initWithZeros(b10,split_index,split_index);
-            initWithZeros(b11,split_index,split_index);
+            int[][] a00 = new int[splitIndex][splitIndex];
+            int[][] a01 = new int[splitIndex][splitIndex];
+            int[][] a10 = new int[splitIndex][splitIndex];
+            int[][] a11 = new int[splitIndex][splitIndex];
+            int[][] b00 = new int[splitIndex][splitIndex];
+            int[][] b01 = new int[splitIndex][splitIndex];
+            int[][] b10 = new int[splitIndex][splitIndex];
+            int[][] b11 = new int[splitIndex][splitIndex];
 
-
-            for (int i = 0; i < split_index; i++){
-                for (int j = 0; j < split_index; j++) {
-                    a00[i][j] = matrix_A[i][j];
-                    a01[i][j] = matrix_A[i][j + split_index];
-                    a10[i][j] = matrix_A[split_index + i][j];
-                    a11[i][j] = matrix_A[i + split_index][j + split_index];
-                    b00[i][j] = matrix_B[i][j];
-                    b01[i][j] = matrix_B[i][j + split_index];
-                    b10[i][j] = matrix_B[split_index + i][j];
-                    b11[i][j] = matrix_B[i + split_index][j + split_index];
+            for (int i = 0; i < splitIndex; i++){
+                for (int j = 0; j < splitIndex; j++) {
+                    a00[i][j] = matrixA[i][j];
+                    a01[i][j] = matrixA[i][j + splitIndex];
+                    a10[i][j] = matrixA[splitIndex + i][j];
+                    a11[i][j] = matrixA[i + splitIndex][j + splitIndex];
+                    b00[i][j] = matrixB[i][j];
+                    b01[i][j] = matrixB[i][j + splitIndex];
+                    b10[i][j] = matrixB[splitIndex + i][j];
+                    b11[i][j] = matrixB[i + splitIndex][j + splitIndex];
                 }
             }
 
-            add_matrix(multiply_matrix(a00, b00),multiply_matrix(a01, b10),result_matrix_00, split_index);
-            add_matrix(multiply_matrix(a00, b01),multiply_matrix(a01, b11),result_matrix_01, split_index);
-            add_matrix(multiply_matrix(a10, b00),multiply_matrix(a11, b10),result_matrix_10, split_index);
-            add_matrix(multiply_matrix(a10, b01),multiply_matrix(a11, b11),result_matrix_11, split_index);
+            addMatrix(multiplyMatrix(a00, b00), multiplyMatrix(a01, b10),resultMatrix_00, splitIndex);
+            addMatrix(multiplyMatrix(a00, b01), multiplyMatrix(a01, b11),resultMatrix_01, splitIndex);
+            addMatrix(multiplyMatrix(a10, b00), multiplyMatrix(a11, b10),resultMatrix_10, splitIndex);
+            addMatrix(multiplyMatrix(a10, b01), multiplyMatrix(a11, b11),resultMatrix_11, splitIndex);
 
-            for (int i = 0; i < split_index; i++){
-                for (int j = 0; j < split_index; j++) {
-                    result_matrix[i][j] = result_matrix_00[i][j];
-                    result_matrix[i][j + split_index] = result_matrix_01[i][j];
-                    result_matrix[split_index + i][j] = result_matrix_10[i][j];
-                    result_matrix[i + split_index] [j + split_index] = result_matrix_11[i][j];
+            for (int i = 0; i < splitIndex; i++){
+                for (int j = 0; j < splitIndex; j++) {
+                    resultMatrix[i][j] = resultMatrix_00[i][j];
+                    resultMatrix[i][j + splitIndex] = resultMatrix_01[i][j];
+                    resultMatrix[splitIndex + i][j] = resultMatrix_10[i][j];
+                    resultMatrix[i + splitIndex] [j + splitIndex] = resultMatrix_11[i][j];
                 }
             }
         }
-        return result_matrix;
+        return resultMatrix;
     }
 
     //Helper methods
-    private static void printMat(int[][] a){
-        for(int i=0; i<a.length;i++){
-            for(int j=0;j<a[0].length;j++){
-                System.out.print(a[i][j]+" ");
+    private static void printMatrix(int[][] matrix){
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j]+ " ");
             }
             System.out.println();
         }
         System.out.println();
     }
 
-    private static void add_matrix(int[][] matrix_A,int[][] matrix_B,int[][] matrix_C, int split_index)
+    private static void addMatrix(int[][] matrixA,int[][] matrixB,int[][] resultMatrix, int splitIndex)
     {
-        for (int i = 0; i < split_index; i++){
-            for (int j = 0; j < split_index; j++){
-                matrix_C[i][j] = matrix_A[i][j] + matrix_B[i][j];
-            }
-        }
-    }
-
-    private static void initWithZeros(int a[][], int r, int c){
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                a[i][j]=0;
+        for (int i = 0; i < splitIndex; i++){
+            for (int j = 0; j < splitIndex; j++){
+                resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
             }
         }
     }
