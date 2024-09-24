22f8cfbed178a06df28d72f6f07971a133aed4d5
David Higgins <david14higgins@gmail.com>
2024-09-24 12:47:19 +0100

    add generate matrix method

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,68 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 
 public class Main {
     public static void main (String[] args) {
-        int[][] matrixA = readMatrixFromFile("8x8_Matrix_A.txt");
-
-        System.out.println("Array A =>");
-        printMatrix(matrixA);
-
-        int[][] matrixB = readMatrixFromFile("8x8_Matrix_B.txt");
-
-        System.out.println("Array B =>");
-        printMatrix(matrixB);
+//        int[][] matrixA = readMatrixFromFile("8x8_Matrix_A.txt");
+//
+//        System.out.println("Array A =>");
+//        printMatrix(matrixA);
+//
+//        int[][] matrixB = readMatrixFromFile("8x8_Matrix_B.txt");
+//
+//        System.out.println("Array B =>");
+//        printMatrix(matrixB);
+//
+//        long sequentialStartTime = System.nanoTime();
+//        SequentialSMM sequentialSMM = new SequentialSMM();
+//        int[][] resultMatrix1 = sequentialSMM.multiply(matrixA, matrixB);
+//        long sequentialEndTime = System.nanoTime();
+//        long sequentialExecutionTime = sequentialEndTime - sequentialStartTime;
+//
+//        long forkStartTime = System.nanoTime();
+//        ForkJoinPoolSMM forkJoinPoolSMM = new ForkJoinPoolSMM();
+//        int[][] resultMatrix2 = forkJoinPoolSMM.execute(matrixA, matrixB);
+//        long forkEndTime = System.nanoTime();
+//        long forkExecutionTime = forkEndTime - forkStartTime;
+//
+//        System.out.println("Result Array =>");
+//        printMatrix(resultMatrix1);
+//        System.out.println();
+//
+//        if (forkExecutionTime > sequentialExecutionTime) {
+//            double multiplier = (double) forkExecutionTime / sequentialExecutionTime;
+//            System.out.println("Sequential time was " + String.format("%.2f", multiplier) + "x faster");
+//        } else {
+//            double multiplier = (double) sequentialExecutionTime / forkExecutionTime;
+//            System.out.println("ForkJoinPool time was " + String.format("%.2f", multiplier) + "x faster");
+//        }
+        int[][] generatedMatrixA = generateRandomMatrix(64, 0, 100);
+        int[][] generatedMatrixB = generateRandomMatrix(64, 0, 100);
 
         long sequentialStartTime = System.nanoTime();
         SequentialSMM sequentialSMM = new SequentialSMM();
-        int[][] resultMatrix1 = sequentialSMM.multiply(matrixA, matrixB);
+        int[][] resultMatrix1 = sequentialSMM.multiply(generatedMatrixA, generatedMatrixB);
         long sequentialEndTime = System.nanoTime();
         long sequentialExecutionTime = sequentialEndTime - sequentialStartTime;
 
         long forkStartTime = System.nanoTime();
         ForkJoinPoolSMM forkJoinPoolSMM = new ForkJoinPoolSMM();
-        int[][] resultMatrix2 = forkJoinPoolSMM.execute(matrixA, matrixB);
+        int[][] resultMatrix2 = forkJoinPoolSMM.execute(generatedMatrixA, generatedMatrixB);
         long forkEndTime = System.nanoTime();
         long forkExecutionTime = forkEndTime - forkStartTime;
 
-        System.out.println("Result Array =>");
-        printMatrix(resultMatrix1);
-        System.out.println();
+        printMatrix(resultMatrix2);
 
+        outputComparison(sequentialExecutionTime, forkExecutionTime);
+    }
+
+    // Helper methods
+    private static void outputComparison(long sequentialExecutionTime, long forkExecutionTime) {
         if (forkExecutionTime > sequentialExecutionTime) {
             double multiplier = (double) forkExecutionTime / sequentialExecutionTime;
             System.out.println("Sequential time was " + String.format("%.2f", multiplier) + "x faster");
@@ -40,11 +70,9 @@ public class Main {
             double multiplier = (double) sequentialExecutionTime / forkExecutionTime;
             System.out.println("ForkJoinPool time was " + String.format("%.2f", multiplier) + "x faster");
         }
-
-
     }
 
-    //IO methods
+    // IO methods
     private static void printMatrix(int[][] matrix){
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
@@ -84,5 +112,19 @@ public class Main {
         matrix = matrixRows.toArray(matrix);
 
         return matrix;
+    }
+
+    // Generates a random matrix where each element is a random value between the upper and lower bound
+    private static int[][] generateRandomMatrix(int dimension, int lowerBound, int upperBound) {
+        int[][] generatedMatrix = new int[dimension][dimension];
+        Random random = new Random();
+
+        for(int i = 0; i < dimension; i++) {
+            for(int j = 0; j < dimension; j++) {
+                generatedMatrix[i][j] = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            }
+        }
+
+        return generatedMatrix;
     }
 }
